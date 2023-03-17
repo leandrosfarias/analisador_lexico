a974a028dd625437ebcefa81d0375753ee1c607d
leandrosfarias <leandro.sfarias997@gmail.com>
2023-03-17 17:07:16 -0300

    Notebook Analisador Léxico

--- Token.py
+++
@@ -1,7 +0,0 @@
-from TokenType import TokenType
-
-
-class Token:
-    def __init__(self, tipo, atributo: str = ''):
-        self.tipo = tipo
-        self.atributo = atributo
--- TokenRegex.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-class Token_regex:
-    TOKEN_REGEX = {
-        'PALAVRAS_RESERVADAS': r"PROGRAM|INTEGER|BOOLEAN|BEGIN|END|WHILE|DO|READ|VAR|FALSE|TRUE|WRITE",
-        'OP_ARITIMETICOS': r"[\+\-\*\/]",
-        'OP_LOGICOS': r"[OR|AND]",
-    }
